4b5e9a5093a4ccd4ede3d2cb77d4ffb8b04d8d72
Cris Schwartz <cris.schwartz0@gmail.com>
2026-07-07 09:50:40 -0500

    tsibble command

--- Scripts/Forecasting_Practice 260707.R
+++ Scripts/Forecasting_Practice 260707.R
@@ -15,3 +15,8 @@ library(fpp3) # load the forecasting package
 #   read_csv("./Data/Student_Pathway_Summary_251114.csv", guess_max = 1000) %>% # guess_max ensures empty rows not treated as logical values 
 #   as_tibble()
 
+y <- tsibble(
+  Year = 2015:2019,
+  Observation = c(123, 39, 78, 52, 110),
+  index = Year
+)
