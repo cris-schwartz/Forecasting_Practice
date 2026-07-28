a850e7609bfb422c5b703bfcf84c77dfc7461341
Cris Schwartz <cris.schwartz0@gmail.com>
2026-07-27 20:22:49 -0500

    practice enrollment data

--- Scripts/Forecasting_Practice 260707.R
+++ Scripts/Forecasting_Practice 260707.R
@@ -10,6 +10,7 @@ rm(list = ls())             ## Clear environment
 pacman::p_load(magrittr, pacman, tidyverse,ggforce)
 library(fpp3) # load the forecasting package
 library(fable)
+library(tidyverse)
 
 # LOAD AND PREPARE DATA ------------------------------------
 # pathway_summary <- # import the previously prepared pathway_summary csv file
@@ -21,17 +22,17 @@ library(fable)
 #   Observation = c(123, 39, 78, 52, 110),
 #   index = Year
 # )
-melsyd_economy <- 
-  ansett %>% 
-  filter(Airports == 'MEL-SYD', Class == "Economy") %>% 
-  mutate(Passengers = Passengers/1000)
-
-print(
-autoplot(melsyd_economy, Passengers) +
-  labs (title = "Ansett airlines economy class",
-        subtitle = "Melbourne-Sydney",
-        y = "Passengers ('000)")
-)
+# melsyd_economy <- 
+#   ansett %>% 
+#   filter(Airports == 'MEL-SYD', Class == "Economy") %>% 
+#   mutate(Passengers = Passengers/1000)
+# 
+# print(
+# autoplot(melsyd_economy, Passengers) +
+#   labs (title = "Ansett airlines economy class",
+#         subtitle = "Melbourne-Sydney",
+#         y = "Passengers ('000)")
+# )
 
 aus_economy <- global_economy |>
   filter(Code == "AUS") |>
@@ -48,3 +49,45 @@ fit <-
 fc <-
   fit %>% 
   forecast(h=10)
+
+# PRACTICE ENROLLMENT TIME SERIES ######
+enrollment_data <- 
+  read_csv("./Data/time_series_dhs 2026-07-26 practice.csv") %>% 
+  as_tibble() %>%
+  select(c(hs_code, starts_with("sem"))) %>% 
+  select(c('hs_code','sem_5', 'sem_8', 'sem_11', 'sem_14',
+           'sem_17', 'sem_20', 'sem_23', 'sem_26',
+           'sem_29', 'sem_32')) %>% 
+  # group_by(hs_code) %>% 
+  select(!'hs_code') %>% 
+  summarize(across(starts_with('sem'), ~sum(.x)))
+
+# enrollment_time_series <- 
+#   enrollment_data %>% 
+#   t() %>% 
+#   as_tibble() %>% 
+#   setNames("enrollment") %>% 
+#   as_tsibble()
+
+enrollment_time_series <- 
+  tibble(
+  sem = (c(5,8,11,14,17,20,23,26,29,32)),
+  enrollment = unlist(enrollment_data, use.names = FALSE))%>% 
+  as_tsibble(index = sem)
+
+new_students <- 
+  enrollment_time_series
+
+autoplot(new_students, enrollment) +
+  labs(y = "New Students", title = "semester code")
+
+enroll_fit <- 
+  new_students %>% 
+  model(
+    AAN = ETS(enrollment ~ error("A") + trend("A") + season("N"))
+  )
+
+enroll_fc <-
+  enroll_fit %>% 
+  forecast(h=10)
+
